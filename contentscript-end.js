f5502f61d75c8646a95b2e37f0bf57d3950f9ec9
Kate Adams <self@kateadams.eu>
2015-06-11 01:27:02 +0100

    Only do times in their own "token". Support hh:mm:ss

--- contentscript-end.js
+++ contentscript-end.js
@@ -1,4 +1,3 @@
-//document.body.innerHTML.replace
 
 function double_digit(number)
 {
@@ -11,27 +10,39 @@ function double_digit(number)
 
 function fix_part(input)
 {
-	var matches = input.match(/(\d+)|(am|pm)/gi)
+	var matches = input.match(/[0-9]+|am|pm/gi)
 	
-	var hh,mm,ap;
-
+	var hh,mm,ss,ap;
+	
+	if(matches == null)
+		return input;
+	
 	if(matches.length == 2) // DD PM
 	{
 		hh = Number(matches[0]);
 		mm = 0;
+		ss = 0;
 		ap = matches[1].toLowerCase().replace(".", "");
 	}
 	else if(matches.length == 3)
 	{
 		hh = Number(matches[0]);
 		mm = Number(matches[1]);
+		ss = 0;
 		ap = matches[2].toLowerCase().replace(".", "");
 	}
+	else if(matches.length == 4)
+	{
+		hh = Number(matches[0]);
+		mm = Number(matches[1]);
+		ss = Number(matches[2]);
+		ap = matches[3].toLowerCase().replace(".", "");
+	}
 	else
-		return;
+		return input;
 	
 	if(hh > 12 )
-		return;
+		return input;
 	
 	if(hh == 12 && ap == "am") // 12am
 		hh = 0;
@@ -41,17 +52,35 @@ function fix_part(input)
 	if(ap == "pm" && hh != 12)
 		hh += 12;
 	
-	return double_digit(hh) + ":" + double_digit(mm);
+	if(matches.length < 4)
+		return double_digit(hh) + ":" + double_digit(mm);
+	else
+		return double_digit(hh) + ":" + double_digit(mm) + ":" + double_digit(ss);
 }
 
-/*$*/(function()
+(function()
 {
+	var count = 0;
+	
 	findAndReplaceDOMText(document, {
-		find: /(\d\d?)(:\d\d)? *(a\.?m\.?|p\.?m\.?)/gi,
+		find: /(^|[^a-z0-9])(\d+)(:\d+){0,2} ?(a\.?m\.?|p\.?m\.?)($|[^a-z0-9])/gi,
 		replace: function(part)
 		{
-			console.log(part);
-			return fix_part(part.text.toLowerCase().replace(".", ""));
+			if(part.index != 1) // 0 = pre, 1 = center, 2 = post
+				return part.text;
+			
+			var text = part.text;
+			
+			text = text
+				.toLowerCase()
+				.replace(".", "");
+			
+			count += 1;
+			return fix_part(text);
 		}
-	})
+	});
+	
+	if(count != 0)
+		console.log("replaced " + count + " 12h times with 24h times");
 })();
+
